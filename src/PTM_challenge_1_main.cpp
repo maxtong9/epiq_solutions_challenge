3a0243beced0f639f4e797fd4e76a174b779eca2
Maxton Ginier <maxginier1@gmail.com>
2023-02-02 19:59:31 +0000

    Challenge 1 main function to run the PTM

---
+++ src/PTM_challenge_1_main.cpp
@@ -0,0 +1,20 @@
+#include "../include/platform_temperature_monitor.hpp"
+using namespace PTM;
+
+/**
+ * @brief Main driver for the PTM Solution utilizing Epiq Solution's challenge criteria
+ */
+int main()
+{
+    // Initialize the temperature monitor with the acceptable temperature boundaries
+    PlatformTemperatureMonitor p1(-40.0, 85.0);
+
+    // Register the four temperature sensors
+    p1.register_temp_sensor(1);
+    p1.register_temp_sensor(2);
+    p1.register_temp_sensor(3);
+    p1.register_temp_sensor(4);
+
+    // Begin monitoring temperatures
+    p1.runTemperatureMonitor();
+}
