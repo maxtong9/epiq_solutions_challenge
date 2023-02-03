e0b5ecdecd9530a7b023f276816df4aa9a05e19f
Maxton Ginier <maxginier1@gmail.com>
2023-02-03 05:06:02 +0000

    Update unit test to create a PTM obj

--- test/platform_temperature_monitor_unit_test.cpp
+++ test/platform_temperature_monitor_unit_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include "platform_temperature_monitor.hpp"
 TEST(test1, testname)
 {
+    PTM::PlatformTemperatureMonitor ptm(-100, 100);
     std::cout << "Hello, World" << std::endl;
     ASSERT_EQ(1, 2);
 }
